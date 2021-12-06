7696f096183f73c9415af5b9b7d8b09f58c9d395
mkoischwitz <mischa.koischwitz@akquinet.de>
2021-12-06 20:13:59 +0100

    IBKM-47 insert graph into dashboard

--- src/sites/dashboard/Home.tsx
+++ src/sites/dashboard/Home.tsx
@@ -1,22 +1,31 @@
 import React, { ReactElement } from 'react';
+import Graph from './Graph';
 
 function Home(): ReactElement {
   const styles = {
-    container: 'w-[calc(100%-3rem)] h-[calc(100%-3rem)] m-6 flex gap-6',
+    container: 'w-[calc(100%-3rem)] h-[calc(100%-3rem)] m-6 flex gap-6 ',
     card: 'shadow-card h-full bg-grayscale-light rounded-[8px] '
   };
   const widgets = [1, 2, 3];
   return <div className={styles.container}>
     {widgets.length > 1 ? (
         <>
-          <span className={styles.card + 'w-2/3'}></span>
+          <span className={styles.card + 'w-2/3'}>
+            <Graph/>
+          </span>
           <div className="w-1/3 h-full flex flex-col gap-6">
-            <span className={styles.card}></span>
-            {widgets[2] && <span className={styles.card}></span>}
+            <span className={styles.card}>
+              <Graph/>
+            </span>
+            {widgets[2] && <span className={styles.card}>
+              <Graph/>
+            </span>}
           </div>
         </>
     ) : (
-        <div className={styles.card + 'w-full'}></div>
+        <span className={styles.card + 'w-full'}>
+          <Graph/>
+        </span>
     )}
   </div>;
 }
