0df1eaf8f04b200d39a303b29dbb0cae0459dfd6
RobinUndersigned <r.decker@undersigned.de>
2021-12-09 21:37:08 +0100

    updates

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
 }
 
 const Button: FC<ButtonProps> = ({onClick, children, variant, isLoading, ...rest}: ButtonProps) => {
-  const stylesPrimary = 'disabled:bg-grayscale-dark disabled:text-grayscale-light hover:bg-primary-light active:bg-primary active:outline-none focus:outline-none focus:ring focus:ring-secondary focus:ring-opacity-20 bg-secondary m-5 text-grayscale-light uppercase px-4 py-2 min-w-[113px] min-h-[43px] rounded-lg text-center tracking-wider font-medium';
+  const stylesPrimary = 'disabled:bg-grayscale-dark disabled:text-grayscale-light hover:focus:active:ring-0 hover:focus:active:outline-none hover:bg-primary-light !hover:ring-0 focus:ring focus:ring-secondary focus:ring-opacity-20 active:bg-primary bg-secondary m-5 text-grayscale-light uppercase px-4 py-2 min-w-[113px] min-h-[43px] rounded-lg text-center tracking-wider font-medium';
   const stylesSecondary = 'disabled:border-2 disabled:border-grayscale-dark disabled:text-grayscale-dark border-secondary hover:border-primary-light text-secondary hover:text-primary-light active:border-primary active:text-primary active:outline-none focus:outline-none focus:ring focus:ring-secondary focus:ring-opacity-20 m-5 border-2 uppercase text-grayscale-light px-4 py-2 min-w-[113px] min-h-[43px] rounded-lg text-center tracking-wider font-medium';
   const stylesText = 'disabled:text-grayscale-dark border-secondary text-secondary hover:text-primary-light active:text-primary active:outline-none focus:outline-none focus:ring focus:ring-secondary focus:ring-opacity-20 m-5 uppercase text-grayscale-light uppercase px-4 py-3 min-w-[113px]  rounded-lg text-center tracking-wider'
 
