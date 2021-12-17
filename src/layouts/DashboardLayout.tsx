91b0dc707f4775d4a6486b20d4fc749f35fa4f71
RobinUndersigned <r.decker@undersigned.de>
2021-12-17 12:01:14 +0100

    review feedback eingearbeitet

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -11,9 +11,9 @@ interface LayoutProps {
 function DashboardLayout(props: LayoutProps): ReactElement {
 
   const styles = {
-    layoutContainer: 'h-full w-full flex flex-col overflow-none relative',
+    layoutContainer: 'h-full w-full flex flex-col overflow-none',
     headerContainer: 'h-14 shadow-lg min-h-14 bg-primary text-grayscale-light px-4 sticky top-0 z-50',
-    mainContainer: 'flex-1 flex gap-4 overflow-hidden',
+    mainContainer: 'flex-1 flex gap-4 overflow-hidden relative',
     sidebarContainer: 'bg-grayscale-light shadow-lg flex flex-col justify-between',
     contentContainer: 'flex-1 overflow-y-scroll relative'
   }
@@ -25,11 +25,11 @@ function DashboardLayout(props: LayoutProps): ReactElement {
   return (
     <ToastContext.Provider value={defaultToastContext}>
       <div className={styles.layoutContainer}>
-        <ToastContainer></ToastContainer>
         <header className={styles.headerContainer}>
           {props.top}
         </header>
         <main className={styles.mainContainer}>
+          <ToastContainer></ToastContainer>
           <div className={styles.sidebarContainer}>{props.left}</div>
           <div className={styles.contentContainer}>{props.center}</div>
         </main>
