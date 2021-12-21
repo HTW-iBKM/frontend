c52effeecc6d1b8fae5e1d83e0b716a585319e72
Kathi <katharina.blass@student.hpi.de>
2021-12-21 22:06:43 +0100

    style graph lines

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -42,6 +42,11 @@ interface GraphDataResponse {
 }
 
 function Graph(): ReactElement {
+  const yIntervall = 500;
+  let maxValue = 0;
+  let minValue = Infinity;
+  const GraphLineColors = ["#4074B2", "#DE9D28", "#edabd1", "#92dbd0"];
+
   const styles = {
     graphContainer:
       "w-[calc(100%-3.5rem)] h-[calc(100%-3.5rem)] m-7 flex justify-center items-center ",
@@ -56,68 +61,104 @@ function Graph(): ReactElement {
     setData(data.data.september_18);
   }, []);
 
-  function calculateDomainMax(data: GraphData[]) {
-    let maxValue = 0;
+  function calculateDomain() {
     let currentMax = 0;
-    data.forEach(element => {
-      currentMax = Math.ceil(Math.max(Number(element.prediction), Number(element.ground_truth)) / 1000) * 1000;
+    let currentMin = Infinity;
+    data.forEach((element) => {
+      currentMax =
+        Math.ceil(
+          Math.max(Number(element.prediction), Number(element.ground_truth)) /
+          yIntervall
+        ) * yIntervall;
+      currentMin = Math.floor(
+        Math.min(Number(element.prediction), Number(element.ground_truth)) /
+        yIntervall
+      ) * yIntervall;
       if (currentMax > maxValue) maxValue = currentMax;
+      if (currentMin < minValue) minValue = currentMin;
     });
-    return maxValue;
+    return [minValue, maxValue];
+  }
+
+  function calculateTickCount() {
+    return maxValue != null && minValue != null ? ((maxValue - minValue) / yIntervall) + 1 : 5;
   }
 
   return !data.length ? (
     <>Waiting for data...</>
-  ) : (
-    <div className={styles.graphContainer}>
-      <ResponsiveContainer>
-        <LineChart
-          data={data.map((entry) => {
-            const newTime = new Date(entry.time);
-            const hours = newTime.toLocaleTimeString().slice(0, 5);
-            return { ...entry, time: hours };
-          })}
-          margin={{ top: 50, right: 50, left: 36 }}
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-          <XAxis
-            dataKey="time"
-            minTickGap={50}
-            interval="preserveStartEnd"
-            tick={{ fontSize: 12, color: "#494B51" }}
-            tickMargin={10}
-            tickSize={8}
-            axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
-            tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
+  ) :
+    (
+      <div className={styles.graphContainer}>
+        <ResponsiveContainer>
+          <LineChart
+            data={data.map((entry) => {
+              const newTime = new Date(entry.time);
+              const hours = newTime.toLocaleTimeString().slice(0, 5);
+              return { ...entry, time: hours };
+            }).slice(0, 50)}
+            margin={{ top: 50, right: 50, left: 36 }}
           >
-            <Label className="axisLabel" value="Zeit" position="right" dx={16} dy={-14} />
-          </XAxis>
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis
+              dataKey="time"
+              minTickGap={50}
+              interval="preserveStartEnd"
+              tick={{ fontSize: 12, color: "#494B51" }}
+              tickMargin={10}
+              tickSize={8}
+              tickCount={calculateTickCount()}
+              axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
+              tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
+            >
+              <Label
+                className="axisLabel"
+                value="Zeit"
+                position="right"
+                dx={16}
+                dy={-14}
+              />
+            </XAxis>
 
-          <YAxis
-            type="number"
-            domain={[0, calculateDomainMax(data)]}
-            allowDecimals={false}
-            minTickGap={50}
-            interval="preserveStartEnd"
-            tickMargin={10}
-            tickSize={8}
-            axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
-            tick={{ fontSize: 12, color: "#494B51" }}
-            tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
-
-          >
-            <Label className="axisLabel" value="Verbrauch in KW" position="top" dy={-16} />
-          </YAxis>
-          <Line name="Prognose" dataKey={"prediction"} stroke="blue" />
-          <Line
-            name="Tatsächlicher Verbrauch"
-            dataKey={"ground_truth"}
-            stroke="red"
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
-  );
+            <YAxis
+              type="number"
+              domain={calculateDomain()}
+              allowDecimals={false}
+              minTickGap={50}
+              interval="preserveStartEnd"
+              tickMargin={10}
+              tickSize={8}
+              tickCount={calculateTickCount()}
+              axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
+              tick={{ fontSize: 12, color: "#494B51" }}
+              tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
+            >
+              <Label
+                className="axisLabel"
+                value="Verbrauch in KW"
+                position="top"
+                dy={-16}
+              />
+            </YAxis>
+            <Line
+              name="Prognose"
+              dataKey={"prediction"}
+              stroke={GraphLineColors[0]}
+              dot={{ fill: GraphLineColors[0], r: 1 }}
+              unit="KW"
+              strokeWidth={1.5}
+            />
+            <Line
+              name="Tatsächlicher Verbrauch"
+              dataKey={"ground_truth"}
+              stroke={GraphLineColors[1]}
+              dot={{ fill: GraphLineColors[1], r: 1 }}
+              unit="KW"
+              strokeWidth={1.5}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
 }
 
 export default Graph;
