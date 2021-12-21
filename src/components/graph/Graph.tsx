e511cce0f165942240943ac9c16bdd066a8a78e9
Kathi <katharina.blass@student.hpi.de>
2021-12-21 21:00:22 +0100

    style Grid and Axis of Graph according to the design

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -1,17 +1,25 @@
-import React, { ReactElement, useState } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
-import axios from 'axios';
+import React, { ReactElement, useState } from "react";
+import {
+  CartesianGrid,
+  Line,
+  Label,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+import axios from "axios";
 import useAsyncEffect from "use-async-effect";
-import './Graph.css';
+import "./Graph.css";
 
 interface GraphData {
   time: string;
   daily_cos: string;
   weekly_sin: string;
   forecast_2: {
-    '0_globalstrahlung': string;
-    '0_temp': string;
-    '0_pressure': string;
+    "0_globalstrahlung": string;
+    "0_temp": string;
+    "0_pressure": string;
   };
   daily_sin: string;
   weekly_cos: string;
@@ -22,56 +30,90 @@ interface GraphData {
   day_hour: number;
   weekday: number;
   sun: number;
-  pressure: number
+  pressure: number;
 }
 
 interface GraphDataResponse {
   data: {
     data: {
-      [x: string]: GraphData[]
-    }
-  }
+      [x: string]: GraphData[];
+    };
+  };
 }
 
 function Graph(): ReactElement {
   const styles = {
-    graphContainer: 'w-[calc(100%-2.5rem)] h-[calc(100%-2.5rem)] m-7 flex justify-center items-center '
+    graphContainer:
+      "w-[calc(100%-3.5rem)] h-[calc(100%-3.5rem)] m-7 flex justify-center items-center ",
   };
   const [data, setData] = useState<GraphData[]>([]);
 
-  useAsyncEffect(async isMounted => {
-    const { data }: GraphDataResponse = await axios.get('https://6ys8ajad27.execute-api.us-east-1.amazonaws.com/');
-    if(!isMounted) return;
+  useAsyncEffect(async (isMounted) => {
+    const { data }: GraphDataResponse = await axios.get(
+      "https://6ys8ajad27.execute-api.us-east-1.amazonaws.com/"
+    );
+    if (!isMounted) return;
     setData(data.data.september_18);
   }, []);
 
-  return (!data.length ? <>Waiting for data...</> :
+  function calculateDomainMax(data: GraphData[]) {
+    let maxValue = 0;
+    let currentMax = 0;
+    data.forEach(element => {
+      currentMax = Math.ceil(Math.max(Number(element.prediction), Number(element.ground_truth)) / 1000) * 1000;
+      if (currentMax > maxValue) maxValue = currentMax;
+    });
+    return maxValue;
+  }
+
+  return !data.length ? (
+    <>Waiting for data...</>
+  ) : (
     <div className={styles.graphContainer}>
       <ResponsiveContainer>
-        <LineChart data={data.map((entry) => {
-          const newTime = new Date(entry.time);
-          const hours = newTime.toLocaleTimeString().slice(0, 5);
-          return ({...entry, time: hours});
-        })}>
-          <CartesianGrid strokeDasharray="3 3"/>
-          <XAxis dataKey="time" label={{value: 'Zeit', position: 'insideBottomRight', dy: 10}}/>
-          <YAxis domain={[0, 2000]} label={{value: 'Verbrauch', position: 'insideLeft', angle: -90, dy: -10}}/>
+        <LineChart
+          data={data.map((entry) => {
+            const newTime = new Date(entry.time);
+            const hours = newTime.toLocaleTimeString().slice(0, 5);
+            return { ...entry, time: hours };
+          })}
+          margin={{ top: 50, right: 50, left: 36 }}
+        >
+          <CartesianGrid strokeDasharray="5 5" />
+          <XAxis
+            dataKey="time"
+            minTickGap={50}
+            interval="preserveStartEnd"
+            tick={{ fontSize: 12, color: "#494B51" }}
+            tickMargin={10}
+            tickSize={8}
+            axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
+            tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
+          >
+            <Label className="axisLabel" value="Zeit" position="right" dx={16} dy={-14} />
+          </XAxis>
 
+          <YAxis
+            type="number"
+            domain={[0, calculateDomainMax(data)]}
+            allowDecimals={false}
+            minTickGap={50}
+            interval="preserveStartEnd"
+            tickMargin={10}
+            tickSize={8}
+            axisLine={{ strokeWidth: 2, stroke: "#494B51" }}
+            tick={{ fontSize: 12, color: "#494B51" }}
+            tickLine={{ strokeWidth: 2, stroke: "#494B51" }}
+
+          >
+            <Label className="axisLabel" value="Verbrauch in KW" position="top" dy={-16} />
+          </YAxis>
+          <Line name="Prognose" dataKey={"prediction"} stroke="blue" />
           <Line
-              name="Prognose"
-              dataKey={'prediction'}
-              stroke="blue"
-              type="monotone"
-              dot={false}
+            name="Tatsächlicher Verbrauch"
+            dataKey={"ground_truth"}
+            stroke="red"
           />
-          <Line
-              name="Tatsächlicher Verbrauch"
-              dataKey={'ground_truth'}
-              stroke="red"
-              type="monotone"
-              dot={false}
-          />
-          <Legend verticalAlign="top"/>
         </LineChart>
       </ResponsiveContainer>
     </div>
