511798dfc27da00e8d182d712142ec0a1e53ce88
RobinUndersigned <r.decker@undersigned.de>
2022-01-06 15:04:36 +0100

    secondary-light gepflegt

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -11,8 +11,8 @@ function Tabs({type, tabs, panels}: TabsProps): ReactElement {
   const styles = {
     tabList: "inline-block ring-1 ring-grayscale-dark border-2 border-grayscale-light rounded-lg",
     tab: {
-      default: "rounded-lg h-9 px-[19px] text-base font-normal leading-7-1/8 hover:bg-primary-light hover:text-grayscale-light active:bg-primary active:text-grayscale-light active:hover:ring-0 focus:outline-none focus:ring-inset focus:ring-4 focus:ring-secondary focus:ring-opacity-20",
-      small: "rounded-lg h-7-1/8 px-4 text-sm font-normal leading-7-1/8 hover:bg-primary-light hover:text-grayscale-light active:bg-primary active:text-grayscale-light focus:outline-none focus:ring-inset focus:ring-4 focus:ring-secondary focus:ring-opacity-20",
+      default: "rounded-lg h-9 px-[19px] text-base font-normal leading-7-1/8 hover:bg-secondary-light hover:text-grayscale-light active:bg-primary active:text-grayscale-light active:hover:ring-0 focus:outline-none focus:ring-inset focus:ring-4 focus:ring-secondary focus:ring-opacity-20",
+      small: "rounded-lg h-7-1/8 px-4 text-sm font-normal leading-7-1/8 hover:bg-secondary-light hover:text-grayscale-light active:bg-primary active:text-grayscale-light focus:outline-none focus:ring-inset focus:ring-4 focus:ring-secondary focus:ring-opacity-20",
       selected: "bg-secondary text-grayscale-light hover:ring-0 focus:ring-0",
       unselected: "bg-transparent text-grayscale-darker"
     }
