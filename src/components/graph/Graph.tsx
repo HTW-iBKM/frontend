b649242d458ec4019c7f347d91dec8eace42306a
mkoischwitz <mischa.koischwitz@akquinet.de>
2022-01-07 01:03:02 +0100

    IBKM-68 review fixes

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -202,11 +202,11 @@ function Graph(): ReactElement {
           )}
         </LineChart>
       </ResponsiveContainer>
-      <div className="border border-[#E2E2E2] w-full m-5"/>
+      <div className="border border-[#E2E2E2] w-full m-6"/>
       <div className="w-full flex justify-center">
         {GraphData.map((data, index) =>
-            <div key={index} className="min-w-max flex items-center gap-3 mx-5">
-              <span className={`w-5 h-5 rounded-[2px]`} style={{backgroundColor: GraphLineColors[index]}}/>
+            <div key={index} className="min-w-max flex items-center gap-3 mx-7">
+              <span className={`w-4 h-4 rounded-[2px]`} style={{backgroundColor: GraphLineColors[index]}}/>
               <span className="text-body1">{data.name}</span>
             </div>
         )}
