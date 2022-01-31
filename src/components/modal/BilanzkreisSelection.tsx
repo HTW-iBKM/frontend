928b1ae7aa39f990678db77b5fd100dac58aceaa
bachdz <xuanbach11696@gmail.com>
2022-01-31 03:35:25 +0100

    rework modal + styling modal
    Former-commit-id: 604c7fd331ac65732173134b3269977b5664c6a3

--- src/components/modal/BilanzkreisSelection.tsx
+++ src/components/modal/BilanzkreisSelection.tsx
@@ -10,133 +10,60 @@ import { v4 as uuidv4 } from 'uuid';
 import "./SaveFileModalTemplate.css";
 import Checkbox from "../form/Checkbox";
 import { useStore } from "../../App";
+import {useCheckbox} from "../../hooks/useCheckbox";
 
 interface BilanzSelectionProps {
     setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-
-
 }
 
+
 const BilanzSelection = ({ setModalOpen }: BilanzSelectionProps): ReactElement => {
+
+    const BALANCE_GROUP = ["Bilanzkreis A", "Bilanzkreis B", "Bilanzkreis C"];
+
     const [bilanzKreise, setBilanzkreise]: any = useStore(state => state.useBilanzKreise);
-    const [checked, setChecked] = useState({
-        option_1: bilanzKreise.length >= 0 ? bilanzKreise[0] : '',
-        option_2: bilanzKreise.length >= 1 ? bilanzKreise[1] : '',
-        option_3: bilanzKreise.length >= 2 ? bilanzKreise[2] : '',
-    });
 
-    // const values = {
-    //     option_1: 'Bilanzkreis 1',
-    //     option_2: 'Bilanzkreis 2',
-    //     option_3: 'Bilanzkreis 3'
-    // }
-
-
-    useEffect(() => {
-        console.log(bilanzKreise);
-    }, [bilanzKreise])
-
-
-
-
-
-    const toastContext = useContext(ToastContext);
-
-    const styles = {
-        formElementGroup: "flex flex-col mb-8 relative"
-    }
-
-    const defaultRadioButtonGroupValue: RadioButtonGroupInterface = {
-        options: ["CSV", "PNG", "PDF"],
-        disabledOptions: ["PDF"],
-        selected: "CSV",
-    }
-
-    const { value: fileName, bind: bindFileName, reset: resetFileName } = useInput("");
-    const {
-        radioButtonGroup: radioButtonGroup,
-        bind: bindRadioButtonGroup,
-        reset: resetRadioButtonGroup
-    } = useRadioButtonGroup(defaultRadioButtonGroupValue);
-
-    const [formTouched, setFormTouched] = useState({
-        name: false,
-        format: false,
+    const balanceGroupObj = BALANCE_GROUP.map((item,index) => {
+        return {
+            name: item,
+            checked: bilanzKreise.includes(item)
+        }
     })
 
-    const formErr: { name: string | null, format: string | null } = {
-        name: !fileName ? "Der Titel muss mindestens einem Buchstaben haben." : null,
-        format: !radioButtonGroup.selected ? 'Wählen sie ein Dateiformat.' : null,
+    const checkboxFormControls = balanceGroupObj.map((item) => {
+        const { checked:checkbox, bind:bindCheckbox, reset:resetCheckbox } = useCheckbox(item.checked)
+        return {name: item.name, checked: checkbox, bind:bindCheckbox, reset:resetCheckbox}
+    });
+
+    const styles = {
+        formElementGroup: "flex flex-col mb-8 relative",
+        description : "my-5"
+
     }
-
-
-
-
-    const validForm = () => {
-        for (const key in checked) {
-            if (checked[key as keyof typeof checked]) return true;
-        }
-        return false;
-    };
-
 
     const handleSubmit = (evt: React.FormEvent) => {
         evt.preventDefault();
-
-        const arr: string[] = []
-        for (const key in checked) {
-            const value = checked[key as keyof typeof checked];
-            if (value) {
-                arr.push(value)
-            }
-        }
-
-
-        setBilanzkreise(arr);
+        setBilanzkreise(checkboxFormControls.filter((checkbox) => checkbox.checked).map(elem => elem.name))
         setModalOpen(false);
     }
 
     return (
         <div>
-            {/*<form name="saveFileForm" onSubmit={handleSubmit}>*/}
-            <p>Wählen Sie alle Bilanzkreise aus, die für Sie relevant sind.</p>
-            <p>Aus dieser Auswahl können Sie mithilfe des Schnellzugriffs in der Navigationsleiste den aktuellen
+            <p className={styles.description}>Wählen Sie alle Bilanzkreise aus, die für Sie relevant sind.</p>
+            <p className={styles.description}>Aus dieser Auswahl können Sie mithilfe des Schnellzugriffs in der Navigationsleiste den aktuellen
                 Bilanzkreis festlegen.</p>
             <form name="bilanceSelection" onSubmit={handleSubmit}>
                 <div className={`${styles.formElementGroup}`}>
-                    <p>
-                        <input className={`${formErr['name'] ? 'border-danger' : ''}`} type="checkbox"
-                            onChange={(e) =>
-                                setChecked((oldState) => ({ ...oldState, option_1: oldState.option_1 === 'Bilanzkreis A' ? '' : 'Bilanzkreis A' }))
-                            }
-                            checked={checked.option_1}
-
-                        />
-                        <label>Bilanzkreis A</label>
-                    </p>
-                    <p>
-                        <input className={`${formErr['name'] ? 'border-danger' : ''}`} type="checkbox"
-                            onChange={(e) =>
-                                setChecked((oldState) => ({ ...oldState, option_2: oldState.option_2 === 'Bilanzkreis B' ? '' : 'Bilanzkreis B' }))
-                            }
-                            checked={checked.option_2}
-                        />
-                        <label>Bilanzkreis B</label>
-                    </p>
-                    <p>
-                        <input className={`${formErr['name'] ? 'border-danger' : ''}`} type="checkbox"
-                            onChange={(e) =>
-                                setChecked((oldState) => ({ ...oldState, option_3: oldState.option_3 === 'Bilanzkreis C' ? '' : 'Bilanzkreis C' }))
-                            }
-                            checked={checked.option_3}
-
-                        />
-                        <label>Bilanzkreis C</label>
-                    </p>
+                    {checkboxFormControls.map((data, index) =>
+                        <div key={index} className="flex gap-2 items-center">
+                            <input id={data.name} type="checkbox" {...data.bind}/>
+                            <label htmlFor={data.name}>{data.name}</label>
+                        </div>
+                    )}
                 </div>
                 <div className={`${commonModalStyles.buttonGroup}`}>
                     <Button variant={"secondary"} onClick={() => setModalOpen(false)}>Abbrechen</Button>
-                    <Button disabled={!validForm()} type="submit" variant={"primary"} >Speichern</Button>
+                    <Button type="submit" variant={"primary"} >Speichern</Button>
                 </div>
             </form>
         </div>
