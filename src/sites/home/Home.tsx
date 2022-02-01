7f6acedeffdae19aa787b8bd42186aada52afdc8
mkoischwitz <mischa.koischwitz@akquinet.de>
2022-02-01 23:48:53 +0100

    IBKM-132 only show 2 widgets if the width is high enough
    Former-commit-id: eba328b936718290f61f259bcd3810b1deeb635a

--- src/sites/home/Home.tsx
+++ src/sites/home/Home.tsx
@@ -10,6 +10,7 @@ import Button from "../../components/form/Button";
 import Modal from "../../components/modal/Modal";
 import BilanzkreisSelection from "../../components/modal/BilanzkreisSelection";
 import {useStore} from "../../App";
+import useWindowDimensions from '../../services/window-dimensions';
 
 function Home(): ReactElement {
     const styles = {
@@ -18,6 +19,8 @@ function Home(): ReactElement {
         option: 'flex h-full flex-col justify-center items-center',
         logo: 'self-center'
     };
+
+    const { width } = useWindowDimensions();
 
     const [exampleFiles, setExampleFiles] = useState(TableMockData());
     const [exampleData, setExampleData] = useState<GraphData[]>([])
@@ -31,8 +34,6 @@ function Home(): ReactElement {
         if (!isMounted) return;
         setExampleData(aiData);
     }, []);
-
-    const widgets = [1, 2];
 
     const columns: Column<FileData>[] = [
         {
@@ -56,20 +57,15 @@ function Home(): ReactElement {
                 </div>
                 :
                 <div className={styles.container}>
-                    {widgets.length > 1 ? (
+                    {width > 1200 ? (
                         <>
                             <div className={styles.card + 'w-2/3'}>
                                 <Graph data={exampleData} header={selectedBilanzKreis + " Graph"} group={selectedBilanzKreis}/>
                             </div>
                             <div className="w-1/3 h-full flex flex-col gap-6">
                                 <div className={styles.card}>
-                                    <TableWidget columns={columns} data={exampleFiles} changeData={setExampleFiles}
-                                                 variant={widgets[2] ? "short" : "long"}/>
+                                    <TableWidget columns={columns} data={exampleFiles} changeData={setExampleFiles} variant={"long"}/>
                                 </div>
-                                {widgets[2] && <div className={styles.card}>
-                                    <TableWidget columns={columns} data={exampleFiles} changeData={setExampleFiles}
-                                                 variant={"short"}/>
-                                </div>}
                             </div>
                         </>
                     ) : (
